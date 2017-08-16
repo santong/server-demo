f155fd3a6ca27788b4c807f21a35a3be926ffaaa
santong <santongdbs@163.comn>
2017-08-16 15:26:40 +0800

    dev: 1

--- config/index.js
+++ config/index.js
@@ -1,8 +1,10 @@
 const development_env = require('./development');
 const test_env = require('./test');
+const product_env = require('./product');
 
 //根据不同的NODE_ENV，输出不同的配置对象，默认输出development的配置对象
 module.exports = {
     development: development_env,
-    test: test_env
+    test: test_env,
+    product: product_env
 }[process.env.NODE_ENV || 'development']
